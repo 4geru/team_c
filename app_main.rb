38c4e2d857adaf63e7bba4c6684b35f68f482c7f
Rp7rf <westhouse51@gmail.com>
2017-01-14 06:58:35 +0000

    複数templateに変更

--- app_main.rb
+++ app_main.rb
@@ -29,27 +29,53 @@ post '/callback' do
       when Line::Bot::Event::MessageType::Text
         message = {
           "type": "template",
-          "altText": "this is a buttons template",
+          "altText": "this is a carousel template",
           "template": {
-              "type": "buttons",
-              "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-              "title": "Menu",
-              "text": "Please select",
-              "actions": [
+              "type": "carousel",
+              "columns": [
                   {
-                    "type": "postback",
-                    "label": "Buy",
-                    "data": "action=buy&itemid=123"
+                    "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+                    "title": "this is menu",
+                    "text": "description",
+                    "actions": [
+                        {
+                            "type": "postback",
+                            "label": "Buy",
+                            "data": "action=buy&itemid=111"
+                        },
+                        {
+                            "type": "postback",
+                            "label": "Add to cart",
+                            "data": "action=add&itemid=111"
+                        },
+                        {
+                            "type": "uri",
+                            "label": "View detail",
+                            "uri": "http://example.com/page/111"
+                        }
+                    ]
                   },
                   {
-                    "type": "postback",
-                    "label": "Add to cart",
-                    "data": "action=add&itemid=123"
-                  },
-                  {
-                    "type": "uri",
-                    "label": "View detail",
-                    "uri": "http://example.com/page/123"
+                    "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+                    "title": "this is menu",
+                    "text": "description",
+                    "actions": [
+                        {
+                            "type": "postback",
+                            "label": "Buy",
+                            "data": "action=buy&itemid=222"
+                        },
+                        {
+                            "type": "postback",
+                            "label": "Add to cart",
+                            "data": "action=add&itemid=222"
+                        },
+                        {
+                            "type": "uri",
+                            "label": "View detail",
+                            "uri": "http://example.com/page/222"
+                        }
+                    ]
                   }
               ]
           }
