8002afbdb78ba238ba98bb7edef310f6ada181a2
sho <sho_ws@outlook.com>
2017-01-14 23:48:15 +0900

    テンプレート追加

--- event_template.rb
+++ event_template.rb
@@ -1,32 +1,32 @@
 def event_template(title, location, fee, body, image, num)
   message = {
-    type: "template",
-    altText: "this is a xarousel template",
-    template: {
-      type: "carousel",
-      columns: [
+    "type": "template",
+    "altText": "this is a xarousel template",
+    "template": {
+      "type": "carousel",
+      "columns": [
         for i in 0..num
           {
-            thumbnailImageUrl: image[i],
-            title: title[i],
-            text: location[i],
-            text: fee[i],
-            text: body[i],
-            actions: [
+            "thumbnailImageUrl": image[i],
+            "title": title[i],
+            "text": location[i],
+            "text": fee[i],
+            "text": body[i],
+            "actions": [
               {
-                type: "postback",
-                label: "OK",
-                data: "action=hoge",
+                "type": "postback",
+                "label": "OK",
+                "data": "action=hoge",
               },
               {
-                type: "postback",
-                label: "detail",
-                data: "action=hoge",
+                "type": "postback",
+                "label": "detail",
+                "data": "action=hoge",
               },
               {
-                type: "postback",
-                label: "post",
-                data: "action=hoge",
+                "type": "postback",
+                "label": "post",
+                "data": "action=hoge",
               }
             ]
           }
