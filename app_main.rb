d8ea683234fb97c4ff996f56ebc268a5ae63fd0d
sho <sho_ws@outlook.com>
2017-01-14 20:31:00 +0900

    テンプレートの追加

--- app_main.rb
+++ app_main.rb
@@ -28,8 +28,57 @@ post '/callback' do
       case event.type
       when Line::Bot::Event::MessageType::Text
         message = {
-          type: 'text',
-          text: event.message['text']
+          "type": "template",
+          "altText": "this is a carousel template",
+          "template": {
+              "type": "carousel",
+              "columns": [
+                  {
+                    "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+                    "title": "this is menu",
+                    "text": "description",
+                    "actions": [
+                        {
+                            "type": "postback",
+                            "label": "Buy",
+                            "data": "action=buy&itemid=111"
+                        },
+                        {
+                            "type": "postback",
+                            "label": "Add to cart",
+                            "data": "action=add&itemid=111"
+                        },
+                        {
+                            "type": "uri",
+                            "label": "View detail",
+                            "uri": "http://example.com/page/111"
+                        }
+                    ]
+                  },
+                  {
+                    "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+                    "title": "this is menu",
+                    "text": "description",
+                    "actions": [
+                        {
+                            "type": "postback",
+                            "label": "Buy",
+                            "data": "action=buy&itemid=222"
+                        },
+                        {
+                            "type": "postback",
+                            "label": "Add to cart",
+                            "data": "action=add&itemid=222"
+                        },
+                        {
+                            "type": "uri",
+                            "label": "View detail",
+                            "uri": "http://example.com/page/222"
+                        }
+                    ]
+                  }
+              ]
+          }
         }
         client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
