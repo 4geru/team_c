870d412276fc13f9adc6a395528abda30e18af74
sho <sho_ws@outlook.com>
2017-01-15 01:37:54 +0900

    テンプレート追加

--- event_template.rb
+++ event_template.rb
@@ -1,6 +1,7 @@
 #def event_template(title, location, fee, body, image, num)
 def event_template
   {
+=begin
     "type": "template",
     "altText": "this is a carousel template",
     "template": {
@@ -36,5 +37,58 @@ def event_template
         #end
        ]
     }
+=end
+
+"type": "template",
+"altText": "this is a carousel template",
+"template": {
+    "type": "carousel",
+    "columns": [
+        {
+          "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+          "title": "this is menu",
+          "text": "description",
+          "actions": [
+              {
+                  "type": "postback",
+                  "label": "Buy",
+                  "data": "action=buy&itemid=111"
+              },
+              {
+                  "type": "postback",
+                  "label": "Add to cart",
+                  "data": "action=add&itemid=111"
+              },
+              {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://example.com/page/111"
+              }
+          ]
+        },
+        {
+          "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+          "title": "this is menu",
+          "text": "description",
+          "actions": [
+              {
+                  "type": "postback",
+                  "label": "Buy",
+                  "data": "action=buy&itemid=222"
+              },
+              {
+                  "type": "postback",
+                  "label": "Add to cart",
+                  "data": "action=add&itemid=222"
+              },
+              {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://example.com/page/222"
+              }
+          ]
+        }
+    ]
+}
   }
 end
