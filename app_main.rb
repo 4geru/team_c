8adf373992a74d8f04c0387efcfe3afc81040be6
Rp7rf <westhouse51@gmail.com>
2017-01-14 06:56:16 +0000

    APIのtemplateに変更

--- app_main.rb
+++ app_main.rb
@@ -28,8 +28,31 @@ post '/callback' do
       case event.type
       when Line::Bot::Event::MessageType::Text
         message = {
-          type: 'text',
-          text: 'hoge'#event.message['text']
+          "type": "template",
+          "altText": "this is a buttons template",
+          "template": {
+              "type": "buttons",
+              "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+              "title": "Menu",
+              "text": "Please select",
+              "actions": [
+                  {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=123"
+                  },
+                  {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=123"
+                  },
+                  {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/123"
+                  }
+              ]
+          }
         }
         client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
