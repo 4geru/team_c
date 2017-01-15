6b9ee544414133040017b302f08cc2b6e7d44bb1
Rp7rf <westhouse51@gmail.com>
2017-01-15 05:40:22 +0000

    カルーセルの追加

--- message.rb
+++ message.rb
@@ -87,26 +87,26 @@ def reply_template_date
 end
 
 def reply_template_contents(category_id=-1,page=0)
-	{
-	  "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
-	  "title": "this is menu",
-	  "text": "description",
-	  "actions": [
-	    {
-	      "type": "postback",
-	      "label": "Buy",
-	      "data": "action=buy&itemid=111"
-	    },
-	    {
-	      "type": "postback",
-	      "label": "Add to cart",
-	      "data": "action=add&itemid=111"
-	    },
-	    {
-	      "type": "uri",
-	  	  "label": "View detail",
-		    "uri": "http://example.com/page/111"
-	    }
-	  ]
-	}
+	 {
+            "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=222"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=222"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/222"
+                }
+            ]
+          }
 end
