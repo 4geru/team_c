a4af1c19db0497180ad73a4d7f426cf91bbd41d1
Rp7rf <westhouse51@gmail.com>
2017-01-15 09:16:41 +0000

    チャンネルシークレットの無効化

--- app_main.rb
+++ app_main.rb
@@ -15,7 +15,7 @@ end
 
 def client
   @client ||= Line::Bot::Client.new { |config|
-    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+#    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
   }
 end
