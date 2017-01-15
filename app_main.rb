3236e1c2e0da6e5f153194fff26e71a9889194df
Rp7rf <westhouse51@gmail.com>
2017-01-15 04:09:04 +0000

    ボタンに反応するようにしました

--- app_main.rb
+++ app_main.rb
@@ -57,6 +57,8 @@ post '/callback' do
         response = client.get_message_content(event.message['id'])
         tf = Tempfile.open("content")
         tf.write(response.body)
+      when Line::Bot::Event::MessageType::Postback
+        client.reply_message(event['replyToken'], reply_message(event.postback['data']))
       end
     end
   }
