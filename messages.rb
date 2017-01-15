69da8b3f32c495d1f1926adaa7372b473c7a01dd
Rp7rf <westhouse51@gmail.com>
2017-01-15 16:19:09 +0000

    とりあえずカルーセル動かせるように

--- messages.rb
+++ messages.rb
@@ -15,13 +15,53 @@ def reply_carousel_museums(museums)
   "type": "template",
   "altText": "this is a carousel template",
   "template": {
-    "type": "carousel",
-    "columns": [
-      reply_carousel_museum_content(museums[0]),
-      reply_carousel_museum_content(museums[1]),
-      reply_carousel_museum_content(museums[2]),
-      reply_carousel_museum_content(museums[3])
-     ]
+      "type": "carousel",
+      "columns": [
+          {
+            "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=111"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=111"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/111"
+                }
+            ]
+          },
+          {
+            "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=222"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=222"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/222"
+                }
+            ]
+          }
+      ]
   }
 }
 end
