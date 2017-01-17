1b5fc4dd4cac2d243b5d5939a16302cabed94e7f
Rp7rf <westhouse51@gmail.com>
2017-01-17 11:19:18 +0000

    dbの設定の変更

--- models/keeps.rb
+++ models/keeps.rb
@@ -1,7 +1,9 @@
 #require 'sqlite3'
 require 'sinatra/activerecord'
 
-ActiveRecord::Base.configurations = YAML.load_file('db/database.yml')
+ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || "sqlite3:db/development.db")
+
+#ActiveRecord::Base.configurations = YAML.load_file('db/database.yml')
 #ActiveRecord::Base.establish_connection(:development)
 
 class Keep < ActiveRecord::Base
