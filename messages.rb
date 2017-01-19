4f3bd9d7d7a8ff7aecafa79482cc000ecede892f
Rp7rf <westhouse51@gmail.com>
2017-01-19 00:54:14 +0000

    confirmの確認

--- messages.rb
+++ messages.rb
@@ -230,12 +230,7 @@ def reply_confirm_gps
 	      "text": "Are you sure?",
 	      "actions": [
 	          {
-	            "id": "325708",
-					    "type": "location",
-					    "title": "my location",
-					    "address": "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
-					    "latitude": 35.65910807942215,
-					    "longitude": 139.70372892916203,
+	          	"type": "message",
 	            "label": "Yes",
 	            "text": "yes"
 	          },
