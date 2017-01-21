d63a55775cf983144bf4d91e41ea9eb2b8dafc2e
Rp7rf <westhouse51@gmail.com>
2017-01-21 02:53:34 +0000

    asoviewカルーセルの返信

--- app_main.rb
+++ app_main.rb
@@ -6,8 +6,8 @@ require './models/keeps'
 require 'json'
 
 get '/' do
-  rand_asoview_genre.to_s
-#  reply_carousel_asoview(rand_asoview_genre).to_s
+#  return rand_asoview_genre.to_s
+  reply_carousel_asoview(rand_asoview_genre).to_s
 #  reply_carousel_museums(reply_museum_datas).to_s
 end
 
